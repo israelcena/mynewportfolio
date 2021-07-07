75eada8e186e7b97994e4ca1cb984a042b1bb1b1
Israel Cena <israelsjm@gmail.com>
2021-07-07 18:05:23 -0300

    change card backgroud color

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -27,6 +27,7 @@ export const BlogCard = styled.div`
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
   width: 400px;
+  background-color: #845EF5;
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 100%;
   }
