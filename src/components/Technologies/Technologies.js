1c0e608bc7dce199819b0c90155968e186e042cd
Israel Cena <israelsjm@gmail.com>
2021-07-07 18:35:35 -0300

    include tecnologies page

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiIllustrator, DiReact, DiNodejs } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <br/>
+    <br/>
+    <SectionTitle>Tecnologias</SectionTitle>
+    <SectionText>
+      Tecnologias que são as minhas paixões no desenvolvimento Web.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="4rem" />
+        <ListContainer>
+          <ListTitle>FrontEnd</ListTitle>
+          <ListParagraph>
+            Atualmente venho focando em React.js, trabalhando em conjunto com NextJS.
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiNodejs size="4rem" />
+        <ListContainer>
+          <ListTitle>BackEnd</ListTitle>
+          <ListParagraph>
+            Seguindo a stack do javascript, Node é a minha liguagem backend favorita.
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiIllustrator size="4rem" />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Protipação de interface e experência do usuário é também umas das minhas skils.
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
