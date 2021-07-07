154c69fb86330c04ffb80586ca79326d1de2a277
Israel Cena <israelsjm@gmail.com>
2021-07-07 16:38:00 -0300

    realocaded page title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,16 +3,16 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillTwitterCircle, AiFillYoutube } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Span, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () => (
   <div>
     <Container>
       <Div1>
         <Link href='/'>
-          <a style={{ display: "flex", alignItems: "center", color: "white" }}>
+          <a style={{ display: "flex", alignItems: "center", justifyContent: "center", color: "white", marginBottom: "20px" }}>
             <DiCssdeck size='5rem' />
-            <span>Portifolio</span>
+            <Span>Israel Cena</Span>
           </a>
         </Link>
       </Div1>
