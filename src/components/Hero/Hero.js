4c09da8894982f2237f39a86b52dff2cc8791470
Israel Cena <israelsjm@gmail.com>
2021-07-07 17:07:01 -0300

    include the main section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,23 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Olá :-) <br/>
+        Meu nome é Israel
+      </SectionTitle>
+      <SectionText>
+      Trabalho como desenvolvedor desde 2007. 
+      Sou formado em <strong>Marketing e Sistemas de Informação</strong>, 
+      apaixonado por tecnologia desde criança. 
+      Acredito que a tecnologia melhora o mundo e nos faz evoluir como sociedade 
+      e que com bastante trabalho e fé tudo é possível!.
+      </SectionText>
+      <Button onClick={()=> window.location = 'https://www.google.com.br'}>Download CV</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
