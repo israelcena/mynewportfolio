d1e60bf38a2b9cdb9e4bfec231abb10bfe0aa27e
Israel Cena <israelsjm@gmail.com>
2021-07-08 12:23:36 -0300

    update content

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,46 +1,54 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: 'Outdoors WebSite',
+    description: "Usando apenas HTML5 E SASS explorei ao máximo esses recursos criando um website perfeito sem a necessidade de usar javascript, provando que toda a aparência do site pode ser feita sem programação.",
+    image: '/images/1.png',
+    tags: ['HTML5', 'CSS3', 'SCSS'],
+    source: 'https://github.com/israelcena/natours',
+    visit: 'https://natours.israelcena.com.br/',
     id: 0,
   },
   {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+    title: 'Trillo Project',
+    description: "Usando apenas FlexBox, construí esse site. Provando que é possível trabalhar apenas com SASS e FlexBox e fazer um site totalmente profissional e bem organizado",
     image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    tags: ['HTML5', 'CSS3', 'SCSS', 'FlexBox'],
+    source: 'https://github.com/israelcena/trillo',
+    visit: 'https://musing-lovelace-93fb14.netlify.app',
     id: 1,
   },
   {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
-      tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: 'NetFlix Clone',
+    description: "Um clone da página principal do antigo site da netflix, neste projeto eu provei mais uma vez que usando apenas html e css é possível fazer um site profissional.",
+    image: '/images/3.png',
+    tags: ['HTML5', 'CSS3'],
+    source: 'https://github.com/israelcena/mynetflixclone',
+    visit: 'https://github.com/israelcena/mynetflixclone',
     id: 2,
   },
   {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+    title: 'Dropbox Clone',
+    description: "Fiz um clone completo, com backend em nodejs com Firebase, usando um servidor Redis para armazenar a secção do usuário e frontend em javascript puro do armazenador de arquivos na nuvem DropBox",
     image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    tags: ['JavaScript', 'NodeJS', 'Firebase', 'Redis'],
+    source: 'https://github.com/israelcena/dropboxclone',
+    visit: 'https://github.com/israelcena/dropboxclone',
     id: 3,
   },
 ];
 
+export const Images = [
+  {
+    name: 'Avatar',
+    image: '/images/avatar.png',
+    description: 'Foto de perfil padrão do Israel Cena'
+  }
+];
+
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2007, text: 'Comecei com meu primeiro Freela', },
+  { year: 2008, text: 'Primeiro emprego como developer', },
+  { year: 2013, text: 'Trabalhando como T.I Administrador', },
+  { year: 2018, text: 'Reposicionamento aprendendo novas Tecnologias', },
+  { year: 2021, text: 'Atualmente aprendendo BackEnd', },
 ];
