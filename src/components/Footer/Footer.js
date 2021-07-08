7565c8294900cd7d46afbc57d38319cea6af0a8a
Israel Cena <israelsjm@gmail.com>
2021-07-08 12:24:25 -0300

    include footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,45 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillLinkedin,AiFillTwitterCircle, AiFillYoutube, AiFillInstagram } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Telefone (C/ Whatsapp)</LinkTitle>
+          <LinkItem href="Tel: +55 22 98803-7449">+55 22 98803-7449</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:israelsjm@gmail.com">israelsjm@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+      <CompanyContainer>
+        <Slogan>Israel Cena - Programador FrontEnd do Rio de Janeiro - Brasil</Slogan>
+      </CompanyContainer>
+      <SocialContainer>
+         <SocialIcons target="_blank" href="https://github.com/israelcena">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons target="_blank" href="https://linkedin.com/in/israelcena">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons target="_blank" href="https://twitter.com/israelcena">
+          <AiFillTwitterCircle size="3rem" />
+        </SocialIcons>
+        <SocialIcons target="_blank" href="https://youtube.com/israelcena">
+          <AiFillYoutube size="3rem" />
+        </SocialIcons>
+        <SocialIcons target="_blank" href="https://instagram.com/israelcena">
+          <AiFillInstagram size="3rem" />
+        </SocialIcons>
+      </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
