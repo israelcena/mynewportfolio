c8ecb3dc0f6407fecdf6a9bb310a9253787e6642
Israel Cena <israelsjm@gmail.com>
2021-08-19 08:43:04 -0300

    change title file location

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
 import Theme from '../styles/theme';
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Israel Cena FrontEnd Dev</title>
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
     </>
   );
 }
- 
