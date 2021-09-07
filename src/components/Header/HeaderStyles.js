cd181b90805bbfcc07741ec9f0c8f51228620662
Israel Cena <israelsjm@gmail.com>
2021-09-07 10:23:31 -0300

    fix header spacing

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -5,9 +5,8 @@ export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-template-rows: 1fr;
-  grid-column-gap: 2rem;
-  padding: 1rem;
-  padding-top: 1rem;
+  grid-column-gap: 1rem;
+  align-items: center;
   margin-bottom: -4rem;
 
   @media ${(props) => props.theme.breakpoints.sm} {
@@ -21,9 +20,8 @@ export const Container = styled.div`
 `;
 
 export const Div1 = styled.div`
-  grid-area: 1 / 1 / 1 / 3;
+  grid-area: 1 / 1 / 1 / 2;
   display: flex;
-  flex-direction: row;
   align-content: center;
   @media ${(props) => props.theme.breakpoints.sm} {
     grid-area: 1 / 1 / 2 / 4;
@@ -34,7 +32,7 @@ export const Div2 = styled.div`
   grid-area: 1 / 2 / 1 / 5;
   display: flex;
   justify-content: space-around;
-  margin-top: 2rem;
+  align-content: center;
   @media ${(props) => props.theme.breakpoints.sm} {
     grid-area: 3 / 2 / 3 / 5;
   }
@@ -52,12 +50,10 @@ export const Div3 = styled.div`
 `;
 
 export const Img = styled.img`
-  width:20%;
-  height:90%;
+  width: 50%;
   border-radius: 50%;
   object-fit: contain;
-  overflow: hidden;
-  margin: 1rem 1rem 0 0;
+  margin-right: 1rem;
 `
 export const Span = styled.span`
   font-size: 1.8rem;
