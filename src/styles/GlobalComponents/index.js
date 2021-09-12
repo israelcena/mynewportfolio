3b72d3d031d5bb2879eb152860907eebeb1d3c3a
Israel Cena <israelsjm@gmail.com>
2021-09-12 15:19:20 -0300

    remove margin

--- src/styles/GlobalComponents/index.js
+++ src/styles/GlobalComponents/index.js
@@ -4,7 +4,7 @@ export const Section = styled.section`
   display: ${(props) => (props.grid ? 'grid' : 'flex')};
   flex-direction: ${(props) => (props.row ? 'row' : 'column')};
   padding: ${(props) => (props.nopadding ? '0' : '32px 48px 0')};
-  margin: 2rem auto;
+  margin: 0 auto;
   max-width: 1040px;
   box-sizing: content-box;
   position: relative;
