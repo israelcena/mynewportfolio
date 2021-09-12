fccfb22063dc488a57a70830b73b11928eaf3332
Israel Cena <israelsjm@gmail.com>
2021-09-12 12:07:07 -0300

    include emphasis in blog button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,45 +1,76 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillTwitterCircle, AiFillYoutube } from 'react-icons/ai';
-import { Container, Span, Div1, Div2, Div3, NavLink, SocialIcons, Img, UlNav } from './HeaderStyles';
-import { Images } from '../../constants/constants';
+import Link from 'next/link'
+import React from 'react'
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+  AiFillYoutube
+} from 'react-icons/ai'
+import {
+  Container,
+  Span,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+  Img,
+  UlNav
+} from './HeaderStyles'
+import { Images } from '../../constants/constants'
 
 const Header = () => (
   <div>
     <Container>
       <Div1>
-        <Link href='/'>
-          <a style={{ display: "flex", alignItems: "center", justifyContent: "start", color: "white" }}>
-            <Img src={Images[0].image}/>
+        <Link href="/">
+          <a
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'start',
+              color: 'white'
+            }}
+          >
+            <Img src={Images[0].image} />
             <div>
               <Span>Israel Cena</Span>
-              <span style={{display: "block", fontSize: "12px"}}>FrontEnd Dev</span>
+              <span style={{ display: 'block', fontSize: '12px' }}>
+                FrontEnd Dev
+              </span>
             </div>
           </a>
         </Link>
       </Div1>
       <Div2>
         <UlNav>
-        <li>
-          <a target="_blank" href="http://blog.israelcena.com.br/"> 
-          <NavLink>Blog</NavLink>
-          </a> 
-        </li>
-        <li>
-          <Link href="#projects"> 
-          <NavLink>Projetos</NavLink>
-          </Link> 
-        </li>
-        <li>
-          <Link href="#tech"> 
-          <NavLink>Tecnologias</NavLink>
-          </Link> 
-        </li>
-        <li>
-          <Link href="#about"> 
-          <NavLink>Sobre</NavLink>
-          </Link>
-        </li>
+          <li
+            style={{
+              backgroundColor: '#9b59b6',
+              padding: '1.5rem',
+              borderRadius: '.7rem'
+            }}
+          >
+            <a target="_blank" href="http://blog.israelcena.com.br/">
+              <NavLink>Blog</NavLink>
+            </a>
+          </li>
+          <li>
+            <Link href="#projects">
+              <NavLink>Projetos</NavLink>
+            </Link>
+          </li>
+          <li>
+            <Link href="#tech">
+              <NavLink>Tecnologias</NavLink>
+            </Link>
+          </li>
+          <li>
+            <Link href="#about">
+              <NavLink>Sobre</NavLink>
+            </Link>
+          </li>
         </UlNav>
       </Div2>
       <Div3>
@@ -57,10 +88,10 @@ const Header = () => (
         </SocialIcons>
         <SocialIcons target="_blank" href="https://instagram.com/israelcena">
           <AiFillInstagram size="4rem" />
-        </SocialIcons>  
+        </SocialIcons>
       </Div3>
     </Container>
   </div>
-);
+)
 
-export default Header;
+export default Header
