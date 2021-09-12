c30cb871071ec280ff173be72e6bdab78dfeb4c9
Israel Cena <israelsjm@gmail.com>
2021-09-12 15:12:29 -0300

    include 2rem for margin

--- src/styles/GlobalComponents/index.js
+++ src/styles/GlobalComponents/index.js
@@ -4,7 +4,7 @@ export const Section = styled.section`
   display: ${(props) => (props.grid ? 'grid' : 'flex')};
   flex-direction: ${(props) => (props.row ? 'row' : 'column')};
   padding: ${(props) => (props.nopadding ? '0' : '32px 48px 0')};
-  margin: 0 auto;
+  margin: 2rem auto;
   max-width: 1040px;
   box-sizing: content-box;
   position: relative;
