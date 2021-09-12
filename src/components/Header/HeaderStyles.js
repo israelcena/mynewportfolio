50f788b9c54750d1ee75631774763a2cea392345
Israel Cena <israelsjm@gmail.com>
2021-09-12 15:46:12 -0300

    change hover transition

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -108,7 +108,7 @@ export const UlNav = styled.ul`
 export const NavLink = styled.a`
   font-size: 2rem;
   color: rgba(255, 255, 255, 0.75);
-  transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease;
 
   &:hover {
     cursor: pointer;
