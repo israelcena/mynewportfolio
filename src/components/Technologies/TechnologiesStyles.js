0b2deceb8544daa7b343ce706cd96ad5b635cc09
Israel Cena <israelsjm@gmail.com>
2021-09-12 15:07:40 -0300

    remove no used style and margin botton for ul

--- src/components/Technologies/TechnologiesStyles.js
+++ src/components/Technologies/TechnologiesStyles.js
@@ -1,49 +1,17 @@
 import styled from 'styled-components'
 
-export const ImageContainer = styled.div`
-  text-align: center;
-  background-image: radial-gradient(
-    50% 50% at 50% 50%,
-    rgba(79, 108, 176, 0.25) 53.8%,
-    rgba(79, 108, 176, 0) 100%
-  );
-  width: 100%;
-  padding: 60px;
-  margin-top: 48px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  @media ${(props) => props.theme.breakpoints.lg} {
-    background-image: none;
-    padding: 0;
-    margin-top: 40px;
-  }
-  @media ${(props) => props.theme.breakpoints.md} {
-    background-image: none;
-    padding: 0;
-    margin-top: 16px;
-  }
-`
-
-export const MainImage = styled.img`
-  width: 100%;
-`
-
 export const List = styled.ul`
   list-style-type: none;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 40px;
-  margin: 3rem 0;
 
   @media ${(props) => props.theme.breakpoints.lg} {
-    margin: 64px 0;
+    margin: 54px 0;
   }
 
   @media ${(props) => props.theme.breakpoints.md} {
-    margin: 64px 0;
+    margin: 44px 0;
     gap: 24px;
   }
 
